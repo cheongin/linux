4be718e32f7f4e738fdbaad7f83fcbc8957fe66a
청인자격증연구소 <115708778+cheongin@users.noreply.github.com>
2024-01-22 17:48:04 +0900

    Update datytime.java

--- Tomcat/datytime.java
+++ Tomcat/datytime.java
@@ -4,7 +4,8 @@ import javax.servlet.http.*;
 import java.util.Calendar;
 
 public class daytime extends HttpServlet {
-    public void doGet(HttpServletRequest request,HttpServletResponse response)
+    public void doGet(HttpServletRequest request
+    ,HttpServletResponse response)
 
     throws IOException, ServletException{
         response.setContentType("text/html");
@@ -12,27 +13,7 @@ public class daytime extends HttpServlet {
         Calendar cal = Calendar.getInstance();
         out.println("<html>\n<head>\n<title>DayTime</title>\n</head>\n<body>");
         out.println("<div style=\"font-size: 40px; text-align: center; font-weight: bold\">");
-        out.println(cal.get(Calendar.YEAR) + "/" + (cal.get(Calendar.MONTH) + 1) + "/" +
-        cal.get(Calendar.DATE) + " " + cal.get(Calendar.HOUR_OF_DAY) + ":" + cal.get(Calendar.MINUTE));
-        out.println("</div>\n</body>\n</html>");
-    }
-}
-
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
-import java.util.Calendar;
-
-public class daytime extends HttpServlet {
-    public void doGet(HttpServletRequest request,HttpServletResponse response)
-
-    throws IOException, ServletException{
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        Calendar cal = Calendar.getInstance();
-        out.println("<html>\n<head>\n<title>DayTime</title>\n</head>\n<body>");
-        out.println("<div style=\"font-size: 40px; text-align: center; font-weight: bold\">");
-        out.println(cal.get(Calendar.YEAR) + "/" + (cal.get(Calendar.MONTH) + 1) + "/" +
+        out.println(cal.get(Calendar.YEAR) + "/" + (cal.get(Calendar.MONTH) + 1) + "/" + 
         cal.get(Calendar.DATE) + " " + cal.get(Calendar.HOUR_OF_DAY) + ":" + cal.get(Calendar.MINUTE));
         out.println("</div>\n</body>\n</html>");
     }
